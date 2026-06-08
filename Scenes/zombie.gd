b44e3046451ae597cb55a91420d0da93321d0eab
shauryaexe1 <shaurya.singhj@gmail.com>
2026-06-08 15:02:34 +1200

    zombie follow working part 1
    still working on zombie follow

--- Scenes/zombie.gd
+++ Scenes/zombie.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
-@onready var target=$Player
-var speed = 100
+@onready var target=$"../Player"
+var speed = 50 
 
 func _physics_process(delta):
-	var direction=(target.position-position).normalized()
+	var direction=(target.global_position-global_position).normalized()
 	velocity=direction * speed
-	look_at (target.position)
+	look_at (target.global_position)
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.

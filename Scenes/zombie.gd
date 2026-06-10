3d19004dd5adf102ca18935e7f7731df45fa786c
shauryaexe1 <shaurya.singhj@gmail.com>
2026-06-10 12:28:47 +1200

    working on player following
    i created the AI which follows the player today

--- Scenes/zombie.gd
+++ Scenes/zombie.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
-@onready var target=$"../Player"
-var speed = 50 
+const speed = 20
 
-func _physics_process(delta):
-	var direction=(target.global_position-global_position).normalized()
-	velocity=direction * speed
-	look_at (target.global_position)
+@export var player: Node2D
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
+func _physics_process(_delta: float) -> void:
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	print(dir)
+	velocity= dir * speed
 	move_and_slide()
 
 
+func makepath() -> void:
+	nav_agent.target_position = player.global_position
+	
+
+
+
+
+
+
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	makepath()
+	

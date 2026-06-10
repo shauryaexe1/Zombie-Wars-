7bf79de5a454d7ef2896d4b28fb7d62e17fe1fc9
shauryaexe1 <shaurya.singhj@gmail.com>
2026-06-11 10:09:19 +1200

    made the zombie animation + player following

--- Scenes/zombie.gd
+++ Scenes/zombie.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
-const speed = 20
+const speed = 40
 
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	print(dir)
 	velocity= dir * speed
+	
+	if velocity.length() > 0:
+		animated_sprite.play("Walk")
+		
 	move_and_slide()
+	
+
 
 
 func makepath() -> void:
 	nav_agent.target_position = player.global_position
-	
+	nav_agent.avoidance_enabled= true
 
 
 

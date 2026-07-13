6db53f3d1bfbbc95c09f39ff69b509c234c23f7c
shauryaexe1 <shaurya.singhj@gmail.com>
2026-07-13 17:19:46 +1200

    working on bullet and shooting mechanism
    today i came back from a week's break and worked on the bullet and gun mechanism. i have decided that within the game now i will be making the player to shoot the gun automatically rather than when clicking. i will work on this in sometime.

--- gun.gd
+++ gun.gd
@@ -6,3 +6,10 @@ func _physics_process(delta):
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+
+func shoot():
+	const BULLET = preload("res://Scenes/bullet.tscn")
+	var new_bullet = BULLET .instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	%ShootingPoint.add_child(new_bullet)

3b3875e81bc253387af88b91ffd9aa87ce15da1f
shauryaexe1 <shaurya.singhj@gmail.com>
2026-08-05 10:31:55 +1200

    finishing the game loading screen
    i worked on the game loading screen along with the game over screen. I need to create the loading screen first so that i could then create the game over screen.

--- main_menu.gd
+++ main_menu.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+
+func _on_game_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
+
+
+func _on_upgrade_menu_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()

bcdf5eb6ada62007cdb95e86ddf3cb43ace510a8
shauryaexe1 <shaurya.singhj@gmail.com>
2026-08-20 10:13:45 +1200

    (almost) finished upgrade menu
    today i almost finished the upgrade menu plus added the main menu attachment to the upgrade menu and game over screen

--- Scenes/game.over.r.gd
+++ Scenes/game.over.r.gd
@@ -8,5 +8,5 @@ func _click_replay() -> void:
 
 
 func _Main_menu() -> void:
-	get_tree()
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	
---
+++ Scripts/upgrade_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _on_fire_rate_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_shield_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_increased_health_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
